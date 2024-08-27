b3074cb4fffca6a6b6d88536d55f9139a07ab88a
Husen Efendi <husenef@gmail.com>
2024-08-27 14:48:52 +0700

    add user api

---
+++ app/api/users/route.ts
@@ -0,0 +1,37 @@
+import { hash } from 'bcryptjs';
+import { prisma } from '@/src/lib/prisma'
+
+//https://nextjs.org/docs/app/building-your-application/routing/route-handlers#request-body
+
+export async function GET() {
+    const users = await prisma.users.findMany()
+    return new Response(JSON.stringify(users), {
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+}
+
+export async function POST(request: Request) {
+    const body = await request.json()
+    const password = await hash(body.password, 12);
+    const user = await prisma.users.create({
+        data: { ...body, password }
+    })
+    return new Response(JSON.stringify(user), {
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+}
+
+export async function DELETE(request: Request, params) {
+    const requestBody = await request.json()
+    console.log({ L30: params });
+
+    return new Response(JSON.stringify(requestBody), {
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+}
